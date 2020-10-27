7ed66e59b4f9130227e44cf1686087b734910087
Eduardo Nascimento da Silva <62409310+EduardoENS@users.noreply.github.com>
2020-10-27 03:52:24 -0300

    Add files via upload

---
+++ trocarValores.js
@@ -0,0 +1,14 @@
+//Mini-Projeto 1 - Trocando Valores de Variáveis
+
+let valor1 = 'azul';
+let valor2 = 'vermelho';
+
+console.log('valor1: ', valor1);
+console.log('valor2: ', valor2);
+
+let aux = valor1;
+valor1 = valor2;
+valor2 = aux;
+
+console.log('valor1: ', valor1);
+console.log('valor2: ', valor2);
