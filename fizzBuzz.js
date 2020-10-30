698eab791c02201f857f37716030acdaf172b940
Eduardo Nascimento da Silva <62409310+EduardoENS@users.noreply.github.com>
2020-10-30 19:18:56 -0300

    Add files via upload

---
+++ fizzBuzz.js
@@ -0,0 +1,29 @@
+/*
+Função FizzBuzz
+Se divisivel por 3, retorna Fizz
+Se divisível por 5, retorna Buzz
+Se divisível por 3 e 5, retorna FizzBuzz
+Não divisível por 3 ou 4, retorna entrada
+Não é número, retorna "Não é um número"
+*/
+
+const resultado = fizzBuzz(7);
+console.log(resultado);
+
+function fizzBuzz(entrada) {
+    if(typeof entrada !== 'number') {
+        return 'Não é um número';
+    }
+    else if(entrada % 3 == 0 && entrada % 5 ==0) {
+        return 'FizzBuzz';
+    }
+    else if(entrada % 5 == 0){
+        return 'Buzz';
+    }
+    else if(entrada % 3 == 0) {
+        return 'Fizz';
+    }
+    else {
+        return entrada;
+    }
+}
---
+++ maior.js
@@ -0,0 +1,8 @@
+// Escreva uma função que usa 2 números e retorna o maior entre eles
+
+let maiorValor = maior(5,6);
+    console.log(maiorValor);
+
+function maior(valor1, valor2) {
+    return valor1 > valor2 ? valor1 : valor2;
+}
